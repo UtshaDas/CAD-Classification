953e86b05e7f8629aabcd0b3e72a23605ee51b8f
UtshaDas <37286540+UtshaDas@users.noreply.github.com>
2020-09-27 12:26:09 +0600

    Add files via upload

---
+++ For All/NB.R
@@ -0,0 +1,39 @@
+rm(list=ls())
+library(e1071)
+
+library(pROC)
+training<-read.csv("F:/Thesis/DataMing+MachieLeaning/CAD/For All/CAD Dataset Train70p.csv")
+testing<-read.csv("F:/Thesis/DataMing+MachieLeaning/CAD/For All/CAD Dataset Test30p.csv")
+
+
+nb_default <- naiveBayes(as.factor(Cath)~., data=training,laplace = 5)
+default_pred <- predict(nb_default, testing[,-67], type="class")
+
+cm=table(default_pred, testing$Cath,dnn=c("Prediction","Actual"))
+
+acc=((sum(diag(cm))/sum(cm)))
+tp<-cm[2,2]
+tn<-cm[1,1]
+fn<-cm[1,2]
+fp<-cm[2,1]
+
+sen=tp/(tp+fn)
+spe=tn/(tn+fp)
+mcc=((tp*tn) - (fp*fn))/(sqrt((tp+fp)*(tp+fn)*(tn+fp)*(tn+fn)))
+f1=2*tp/((2*tp)+fp+fn)
+
+roc_obj<-roc(testing[,67],as.numeric(default_pred))
+rocauc<-auc(roc_obj)
+
+print('Accuracy')
+print(acc)
+print('sensitivity')
+print(sen)
+print('Specificity')
+print(spe)
+print('MCC')
+print(mcc)
+print('F1')
+print(f1)
+print('AUC')
+print(rocauc)
---
+++ For All/RandomForest.R
@@ -0,0 +1,44 @@
+library(randomForest)
+library(pROC)
+trainDF<-read.csv("F:/Thesis/DataMing+MachieLeaning/CAD/For All/CAD Dataset Train70p.csv")
+testDF<-read.csv("F:/Thesis/DataMing+MachieLeaning/CAD/For All/CAD Dataset Test30p.csv")
+
+trainDF$Cath<-as.factor(trainDF$Cath)
+testDF$Cath<-as.factor(testDF$Cath)
+
+bestmtry<-tuneRF(trainDF,as.factor(trainDF$Cath),ntreeTry = 50,stepFactor =1.2,
+                 improve = 0.01,trace = TRUE,plot = TRUE, doBest=TRUE)
+
+modelRandom<-randomForest(Cath~.,data = trainDF,mtry=bestmtry$mtry,ntree=bestmtry$ntree) 
+#print(importance(modelRandom))
+#varImpPlot(modelRandom)
+PredictionsWithClass<- predict(modelRandom, testDF[,1:66], type = 'class')
+cm<-table(predictions=PredictionsWithClass, actual=testDF$Cath)
+
+acc=((sum(diag(cm))/sum(cm)))
+tp<-cm[2,2]
+tn<-cm[1,1]
+fn<-cm[1,2]
+fp<-cm[2,1]
+
+sen=tp/(tp+fn)
+spe=tn/(tn+fp)
+mcc=((tp*tn) - (fp*fn))/(sqrt((tp+fp)*(tp+fn)*(tn+fp)*(tn+fn)))
+f1=2*tp/((2*tp)+fp+fn)
+
+roc_obj<-roc(testDF$Cath,as.numeric(PredictionsWithClass))
+rocauc<-auc(roc_obj)
+
+
+print('Accuracy')
+print(acc)
+print('sensitivity')
+print(sen)
+print('Specificity')
+print(spe)
+print('MCC')
+print(mcc)
+print('F1')
+print(f1)
+print('AUC')
+print(rocauc)
